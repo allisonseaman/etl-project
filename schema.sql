d4d1226f87d651ab9e9328da91e4f1d3c6c36396
allisonseaman <53864946+allisonseaman@users.noreply.github.com>
2019-11-13 20:05:02 -0600

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,8 @@
+-- Create tables for raw data to be loaded into
+CREATE TABLE crime_data (
+id INT PRIMARY KEY,
+Date date,
+"Primary Type" VARCHAR(100),
+"Description" VARCHAR(100)
+);
+
---
+++ table.sql
@@ -0,0 +1,9 @@
+-- Create tables for raw data to be loaded into
+CREATE TABLE crime_data (
+id INT PRIMARY KEY,
+Date date,
+"Primary Type" VARCHAR(100),
+"Description" VARCHAR(100)
+);
+
+SELECT * from crime_data;
